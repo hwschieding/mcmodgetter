55a7af8395df31b78464f124f4260f0ff6a1ebf6
hwschieding <hwschieding@gmail.com>
2025-10-31 14:43:34 -0400

    get multiple projects + tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use futures::future;
 use serde::Deserialize;
 
 static APP_USER_AGENT: &str = concat!(
@@ -32,8 +33,42 @@ pub fn create_client() -> Result<reqwest::Client, reqwest::Error> {
         .build()
 }
 
-pub async fn get_project(client: &reqwest::Client, id: &String) -> Result<Project, reqwest::Error> {
+pub async fn get_project(client: &reqwest::Client, id: &str) -> Result<Project, reqwest::Error> {
     let url = format!("{}{}{}", MODRINTH_URL, "/v2/project/", id);
     let response = client.get(url).send().await?;
     response.json::<Project>().await
+}
+
+pub async fn get_projects_from_list(
+    client: &reqwest::Client,
+    ids: &Vec<String>
+) -> Vec<Result<Project, reqwest::Error>>
+{
+    let mut responses = Vec::new();
+    for id in ids {
+        responses.push(get_project(client, id));
+    }
+    future::join_all(responses).await
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn get_single_project() {
+        let client = create_client().expect("Client should be created");
+        let project = get_project(&client, "AANobbMI").await.expect("should exist");
+        assert_eq!(project.title, "Sodium");
+        assert_eq!(project.id, "AANobbMI")
+    }
+
+    #[tokio::test]
+    async fn get_list_of_projects() {
+        let client = create_client().expect("Client should be created");
+        let ids_vec = vec![String::from("P7dR8mSH"), String::from("AANobbMI"), String::from("9s6osm5g")];
+        let project_vec = get_projects_from_list(&client, &ids_vec).await;
+        assert_eq!(project_vec[0].as_ref().expect("Should exist").title, "Fabric API");
+        assert_eq!(project_vec[1].as_ref().expect("Should exist").title, "Sodium")
+    }
 }
